8f055602c9097608b51bed46d1354ef2c7f5cbd9
Alexey <alexey_kuzminykh@mail.ru>
2022-12-08 14:08:16 +0300

    Initial commit

---
+++ src/main/java/my/sky/web_application_structure/WebApplicationStructureApplication.java
@@ -0,0 +1,13 @@
+package my.sky.web_application_structure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationStructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplicationStructureApplication.class, args);
+    }
+
+}
